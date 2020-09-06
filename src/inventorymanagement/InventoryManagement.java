f1feb9a0807e98e3346fe24e554dc74428d91ed1
KRISHANI <KRISHANI@LAPTOP-VASCM6FJ>
2020-09-06 15:18:18 +0530

    inventory

---
+++ src/inventorymanagement/InventoryManagement.java
@@ -0,0 +1,23 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package inventorymanagement;
+/**
+ *
+ * @author KRISHANI
+ */
+public class InventoryManagement {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        homepage obj = new homepage();
+        obj.setSize(1365,750);
+        obj.setVisible(true);
+    }
+    
+}
